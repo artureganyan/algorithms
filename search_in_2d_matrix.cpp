fc2afbcf9b5b36a8d34a67cb6de28671b892a64e
artureganyan <artureganyan@users.noreply.github.com>
2019-11-18 22:26:44 +0300

    added more solutions to search_in_2d_matrix

--- search_in_2d_matrix.cpp
+++ search_in_2d_matrix.cpp
@@ -6,12 +6,17 @@
 
 namespace search_in_2d_matrix {
 
-class Solution {
+// Uses standard algorithms
+
+class Solution1 {
 public:
-    // Time: O(log(n)), Space: O(1)
+    // Time: O(log(n)), Space: O(1), n - number of the matrix elements
     //
     // Note: Each row must be sorted in ascending order, and its first cell
     // must be greater than the last cell of the previous row.
+    //
+    // Note: Time complexity is log(rows_count) + log(columns_count) =
+    // log(rows_count * columns_count) = log(n).
     //
     bool run(const std::vector<std::vector<int>>& matrix, int target)
     {
@@ -33,16 +38,192 @@ public:
 };
 
 
+// Uses manually written binary search
+
+class Solution2 {
+public:
+    // Time: O(log(n)), Space: O(1), n - number of the matrix elements
+    //
+    // Note: Each row must be sorted in ascending order, and its first cell
+    // must be greater than the last cell of the previous row.
+    //
+    bool run(const std::vector<std::vector<int>>& matrix, int target)
+    {
+        if (!matrix.size() || !matrix[0].size())
+            return false;
+
+        const int rows_count = matrix.size();
+        const int cols_count = matrix[0].size();
+
+        int i1 = 0;
+        int i2 = rows_count * cols_count - 1;
+
+        while (i1 <= i2) {
+            const int i = i1 + (i2 - i1) / 2;
+            const int r = i / cols_count;
+            const int c = i % cols_count;
+
+            const int value = matrix[r][c];
+            if (value < target) {
+                i1 = i + 1;
+            } else if (value > target) {
+                i2 = i - 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+
+// Uses the standard binary search with the custom iterator
+
+class Solution3 {
+public:
+    // Time: O(log(n)), Space: O(1), n - number of the matrix elements
+    //
+    // Note: Each row must be sorted in ascending order, and its first cell
+    // must be greater than the last cell of the previous row.
+    //
+    bool run(const std::vector<std::vector<int>>& matrix, int target)
+    {
+        if (!matrix.size() || !matrix[0].size())
+            return false;
+
+        const MatrixConstIterator begin(&matrix, 0, 0);
+        const MatrixConstIterator end(&matrix, matrix.size(), 0);
+
+        return std::binary_search(begin, end, target);
+    }
+
+private:
+    typedef std::vector<std::vector<int>> Matrix;
+
+    class MatrixConstIterator {
+    public:
+        typedef int difference_type;
+        typedef int value_type;
+        typedef int* pointer;
+        typedef int& reference;
+        typedef std::random_access_iterator_tag iterator_category;
+
+        MatrixConstIterator()
+            : matrix_(nullptr), r_(0), c_(0), columns_count_(0) {}
+
+        MatrixConstIterator(const Matrix* matrix, size_t r, size_t c)
+            : matrix_(matrix), r_(r), c_(c), columns_count_((*matrix)[0].size()) {}
+
+        bool operator ==(const MatrixConstIterator& other) const
+        {
+            return matrix_ == other.matrix_ && r_ == other.r_ && c_ == other.c_;
+        }
+
+        bool operator !=(const MatrixConstIterator& other) const
+        {
+            return !(*this == other);
+        }
+
+        MatrixConstIterator operator +(size_t n) const
+        {
+            MatrixConstIterator it = *this;
+            it += n;
+            return it;
+        }
+
+        MatrixConstIterator operator -(size_t n) const
+        {
+            MatrixConstIterator it = *this;
+            it -= n;
+            return it;
+        }
+
+        int operator -(const MatrixConstIterator& other) const
+        {
+            if (matrix_ != other.matrix_)
+                throw std::runtime_error("Incompatible MatrixConstIterators");
+
+            return (r_ - other.r_) * columns_count_ + (c_ - other.c_);
+        }
+
+        MatrixConstIterator& operator +=(size_t n)
+        {
+            c_ += n;
+            if (c_ >= columns_count_) {
+                r_ += c_ / columns_count_;
+                c_  = c_ % columns_count_;
+            }
+            return *this;
+        }
+
+        MatrixConstIterator& operator -=(size_t n)
+        {
+            return operator +=(-n);
+        }
+
+        const int& operator [](size_t i) const
+        {
+            return *(*this + i);
+        }
+
+        const int& operator *() const
+        {
+            return (*matrix_)[r_][c_];
+        }
+
+        MatrixConstIterator& operator ++()
+        {
+            return *this += 1;
+        }
+
+        MatrixConstIterator operator ++(int /*post-increment*/)
+        {
+            const MatrixConstIterator prev = *this;
+            operator ++();
+            return prev;
+        }
+
+        MatrixConstIterator& operator --()
+        {
+            return *this -= 1;
+        }
+
+        MatrixConstIterator operator --(int /*post-decrement*/)
+        {
+            const MatrixConstIterator prev = *this;
+            operator --();
+            return prev;
+        }
+
+        bool operator <(const MatrixConstIterator& other) const
+        {
+            if (matrix_ != other.matrix_)
+                throw std::runtime_error("Incompatible MatrixConstIterators");
+
+            return r_ < other.r_ || c_ < other.c_;
+        }
+
+    private:
+        const Matrix* matrix_;
+        size_t        columns_count_;
+        size_t        r_;
+        size_t        c_;
+    };
+};
+
+
 typedef std::vector<std::vector<int>> Matrix;
 
-void test(const Matrix& matrix, int target, bool expected)
+template <typename Solution>
+void test()
 {
-    ASSERT_EX( Solution().run(matrix, target) == expected,
-               to_string(matrix) + ", " + to_string(target) + " == " + to_string(expected) );
-}
+    auto test = [](const Matrix& matrix, int target, bool expected)
+    {
+        ASSERT_EX( Solution().run(matrix, target) == expected,
+                   to_string(matrix) + ", " + to_string(target) + " == " + to_string(expected) );
+    };
 
-int main()
-{
     Matrix m;
 
     test({}, 0, false);
@@ -87,6 +268,35 @@ int main()
     test(m, 14, false);
     test(m, 20, true);
     test(m, 21, false);
+
+
+    // Test performance
+    const int size = 1000;
+    const int value_count = size * size;
+
+    m = Matrix(size, std::vector<int>(size, 0));
+    for (int r = 0, v = 0; r < size; r++) {
+        for (int c = 0; c < size; c++, v++) {
+            m[r][c] = v;
+        }
+    }
+
+    std::cout << "Run for " << value_count << " elements" << std::endl;
+
+    clock_t start = clock();
+    for (int i = 0; i < value_count; i++)
+        Solution().run(m, i);
+
+    double elapsed_secs = double(clock() - start) / CLOCKS_PER_SEC;
+    std::cout << "Elapsed: " << elapsed_secs << " seconds" << std::endl;
+    std::cout << std::endl;
+}
+
+int main()
+{
+    test<Solution1>();
+    test<Solution2>();
+    test<Solution3>();
 
     return 0;
 }

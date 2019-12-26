9d8c7fe2964f064e512635c4a8559d3a47a4f80e
artureganyan <artureganyan@users.noreply.github.com>
2019-12-26 11:03:12 +0300

    added populating_next_right_pointers_in_each_node_2

---
+++ populating_next_right_pointers_in_each_node_2.cpp
@@ -0,0 +1,9 @@
+// Problem: https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+
+#include "utils.h"
+
+namespace populating_next_right_pointers_in_each_node_2 {
+
+// See populating_next_right_pointers_in_each_node
+
+}

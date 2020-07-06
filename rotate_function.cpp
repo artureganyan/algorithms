6b395b8f4632fadd6c7954a8099b46836c6f3f02
artureganyan <artureganyan@users.noreply.github.com>
2020-07-06 16:50:38 +0300

    updated solution for rotate_function

--- rotate_function.cpp
+++ rotate_function.cpp
@@ -11,43 +11,55 @@ public:
     // Note: If the values is empty, returns 0. The result and the sum of all
     // the values must fit into int.
     //
-    // Time: O(n), Space: O(n), n - number of integers
+    // Time: O(n), Space: O(1), n - number of integers
     //
     int run(const std::vector<int>& values)
     {
+        // Idea:
+        // We need to find the maximum sum
+        //
+        // s(v)   = 0*v[0] + 1*v[1] + 2*v[2] + ... + (n-1)*v[n-1]
+        //
+        // among all rotations of v. Calculating such a sum takes O(n) time, so
+        // it takes O(n^2) for the brute-force solution. But we may notice that,
+        // given the current rotation of v, the sum for the next rotation can
+        // be calculated recurrently:
+        //
+        // s(v,0) = 0*v[0] + 1*v[1] + 2*v[2] + ... + (n-1)*v[n-1]
+        // s(v,1) =          0*v[1] + 1*v[2] + ... + (n-2)*v[n-1] + (n-1)*v[0] =
+        //          s(v,0) - ( v[1] +   v[2] + ... +       v[n-1])+ (n-1)*v[0]
+        //
+        // i.e. as the current sum minus all values except the first one, plus
+        // this first value multiplied by n-1. So we could prepare these
+        // partial sums ("all values except i-th") in O(n) time and O(n) space,
+        // and then find the maximum in O(n) time. But since each partial sum
+        // is the total sum minus the i-th value, this can be simplified:
+        //
+        // s(v,1) = s(v,0) - ( v[1] +   v[2] + ... +       v[n-1] + v[0]) + n*v[0]
+        //
+        // So we always subtract all values and just change the miltiplier from
+        // (n-1) to n. Now, after precalculating the sum of all values in O(n)
+        // time and O(1) space, we can find the maximum in O(n) time.
+
         if (!values.size())
             return 0;
 
-        // Prepare partial sums
-        struct PartialSum
-        {
-            int left;
-            int right;
-        };
+        // Calculate sum of all values
+        int total_sum = 0;
+        for (int i = 0; i < values.size(); i++)
+            total_sum += values[i];
 
-        std::vector<PartialSum> sums(values.size(), {0, 0});
-        PartialSum              sum = {0, 0};
+        // Calculate initial sum
+        int sum = 0;
+        for (int i = 0; i < values.size(); i++)
+            sum += values[i] * i;
 
+        // Find maximum sum among all rotations of the values
+        int max_sum = sum;
         for (int i = 1; i < values.size(); i++) {
-            sum.left += values[i - 1];
-            sums[i].left = sum.left;
-        }
-        for (int i = values.size() - 1; i >= 0; i--) {
-            sum.right += values[i];
-            sums[i].right = sum.right;
-        }
-
-        // Prepare initial left/right sums
-        sum = {0, 0};
-        for (int i = 1; i < values.size(); i++)
-            sum.right += values[i] * i;
-
-        // Find maximum rotated sum
-        int max_sum = sum.left + sum.right;
-        for (int i = 1; i < values.size(); i++) {
-            sum.left  += -sums[i].left + values[i - 1] * values.size();
-            sum.right -= sums[i].right;
-            max_sum    = std::max(max_sum, sum.left + sum.right);
+            sum -= total_sum;
+            sum += values[i - 1] * values.size();
+            max_sum = std::max(max_sum, sum);
         }
         return max_sum;
     }
@@ -104,7 +116,7 @@ int main()
     test({MIN});
     test({0, MIN});
 
-    // Large input
+    // Large random input
     std::vector<int> large_input;
     for (int i = 0; i < 10000; i++)
         large_input.push_back(rand() % 10 - rand() % 10);
